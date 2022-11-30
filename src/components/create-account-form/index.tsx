1515524bf6fa9b47d73704aeb1086d92589915d0
graceizabayo <graceizabayo1@gmail.com>
2022-11-30 17:42:09 +0200

    modifying index.tsx

--- src/components/create-account-form/index.tsx
+++ src/components/create-account-form/index.tsx
@@ -4,19 +4,19 @@ const CreateAccountForm = () =>{
     return <div className="create-account-form">
         <h2>Create Account</h2>
         <p className="pi">Create a great platform for managing your cases & clients</p>
+        <br />
         <div className="test1">
-        <TextFieldInput name="fullName" label="Full name" />
-        <TextFieldInput name="Email
+        <TextFieldInput name="fullName" label="Full name*" />
+        <TextFieldInput name="Organization name" label="Organization name " />
         
-        " label="Email" />
         </div>
          <div className="test2">
-        <TextFieldInput name="Organization name" label="Organization name " />
+         <TextFieldInput name="Email " label="Email  Address*" />
         <TextFieldInput type="number"name="Mobile Number" label="Mobile Number" />
         
     </div>
     <div className="pass">
-    <TextFieldInput  type="password" name="password" label="Password" />
+    <TextFieldInput  type="password" name="password" label="Password*" />
     </div>
     <button className="btn">SIGN UP</button>
      </div>
