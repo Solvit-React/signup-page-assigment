8fc55fb8266b61055df851a1b3ecf2cd909bb08e
Agnes IRADUKUNDA <kundaaggy@gmai.com>
2022-11-30 09:27:43 +0200

    Simple Signup Form using react.js

--- src/components/create-account-form/index.tsx
+++ src/components/create-account-form/index.tsx
@@ -1,10 +1,29 @@
 import TextFieldInput from "../text-field";
 import "./style.css";
 const CreateAccountForm = () =>{
-    return <div className="create-account-form">
-        <TextFieldInput name="fullName" label="Full name" />
-        <TextFieldInput name="Emai" label="Email" />
-    </div>
+    return (<div className="create-account-form">
+         <div className='header-text'>
+            <h1>Create Account</h1>
+            <p>Create a great platform for managing your cases & clients</p>
+        </div>
+        <form action="" className='form'>
+        <div className="container">
+        <TextFieldInput name="fullName"  type="text" label="Full name*" />
+        <TextFieldInput name="Organisation"  type="text" label="organisation name*" />
+</div>
+<div className="container">
+        <TextFieldInput name="Email"  type="email"label="Email address*" />
+        
+        <TextFieldInput name="telphone number" label="telphone number*"type="number " />
+        </div>
+        <div className="container1">
+        <TextFieldInput name="password" label="password*" type="password" /> 
+            </div>
+            </form>
+        <button className='signup-btn'>SIGN UP</button>
+        
+    
+    </div>)
 }
 
 export default CreateAccountForm;
--- src/components/dev-image/index.tsx
+++ src/components/dev-image/index.tsx
@@ -1,8 +1,11 @@
 import "./style.css";
-
+import image from "../../assets/image.png"
 const DevImage = () => {
-    return <div className="dev-image">
-        <h1>Dev imag</h1>
+    return(<div className="dev-image">
+        <span className='login-text'>Already a member?</span>
+        <button className='login-btn'>LOGIN</button>
+        <img src={image} alt="" />
     </div>
+    )
 }
 export default DevImage;
